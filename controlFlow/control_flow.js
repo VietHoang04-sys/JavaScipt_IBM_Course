0f4fe323ec18a708f9332c758297b3a2486e1cda
HoangVietVuong <vvhoang.884@gmail.com>
2024-11-22 22:47:15 -0500

    add control flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,12 @@
+let userRole = "admin";
+let isLoggedIn = true;
+let userMessage;
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
